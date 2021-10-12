e00e86b9e7eb9322e49642661f02571ab66b8e25
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-11 23:02:17 -0300

    first commit

---
+++ web/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+class Diarista(models.Model):
+  nome_completo = models.CharField(
+    max_length=100,
+  )
+
+  cpf = models.CharField(
+    max_length=11,
+    unique=True
+  )
+
+  email = models.EmailField(
+    max_length=100,
+    unique=True
+  )
+
+  telefone = models.CharField(
+    max_length=11
+  )
+
+  logradouro = models.CharField(
+    max_length=60
+  )
+
+  numero = models.IntegerField()
+
+  bairro = models.CharField(
+    max_length=30
+  )
+
+  complemento = models.CharField(
+    max_length=100,
+    blank=True
+  )
+
+  cep = models.CharField(
+    max_length=8
+  )
+
+  estado = models.CharField(max_length=2)
+
+  codigo_ibge = models.IntegerField()
+
+  foto_usuario = models.ImageField()
